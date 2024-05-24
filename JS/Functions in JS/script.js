e5cacd3b423927fd025687ce0c516b0a2fab4167
Shrey <mishra.shrey001@gmail.com>
2024-05-24 10:45:52 +0530

    again commiting first time

---
+++ JS/Functions in JS/script.js
@@ -0,0 +1,11 @@
+function myfunction(x, y)
+{
+  if (y=== undefined) {
+              y = 2;
+                 }  
+
+                 return x*y;
+   
+}
+
+console.log(myfunction(5));
---
+++ JS/Intro To JS/scrpt.js
@@ -0,0 +1,6 @@
+
+num1 = 5;
+var num2 = 6;
+var sum = num1 + num2;
+document.getElementById("result").innerHTML ="<b>The total sum is: </b>" + sum;
+
